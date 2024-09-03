ce687f00b2151da856c1b43f504e42bf3c4da324
Dragos Ungureanu <ungureanud6@gmail.com>
2024-09-03 07:46:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import AddTransactionForm from "./components/AddTransactionForm/AddTransactionForm";
 
 function App() {
   return (
     <>
-      {" "}
       <h1>Money Heist</h1>
-      <AddTransactionForm />
     </>
   );
 }
