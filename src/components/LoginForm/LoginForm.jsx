d11aa2a0cdfd6fd2464b8df329d3a810c69da516
Dragos Ungureanu <ungureanud6@gmail.com>
2024-09-09 21:58:47 +0200

    required messages + some hooks for login and register

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -11,10 +11,21 @@ import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 import styles from "./LoginForm.module.css";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import useToggle from "../../hooks/useToggle";
+import useFormValidation from "../../hooks/useFormValidation";
+import validateLogin from "../../hooks/validateLogin";
+import useFormTouched from "../../hooks/useFormTouched";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { fields, setFields, validateFields } = useFormValidation(
+    {
+      email: "",
+      password: "",
+    },
+    validateLogin
+  );
+
+  const { touched, handleBlur } = useFormTouched(fields);
+
   const [errorMessage, setErrorMessage] = useState("");
   const dispatch = useDispatch();
 
@@ -22,77 +33,95 @@ function LoginForm() {
   const [eyeVisible, toggleEyeVisible] = useToggle(true);
   const [closedEyeVisible, toggleClosedEyeVisible] = useToggle(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(logIn({ email, password }))
-      .unwrap()
-      .then(() => {})
-      .catch((error) => {
-        setErrorMessage(
-          error.message || "You have entered an invalid username or password."
-        );
-      });
+
+    if (!validateFields()) return;
+
+    try {
+      await dispatch(logIn(fields)).unwrap();
+    } catch (error) {
+      setFields((prevFields) => ({
+        ...prevFields,
+        errorMessage: setErrorMessage(
+          "You have entered an invalid username or password."
+        ),
+      }));
+    }
   };
 
   return (
     <>
       <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.inputWrapper}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.inputIcon} />
+        <div className={styles.inputContainer}>
+          <div className={styles.inputWrapper}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.inputIcon} />
 
-          <Input
-            autoComplete="on"
-            paddingLeft="53.5px"
-            width="100%"
-            type="email"
-            value={email}
-            handleChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            placeholder="E-mail"
-            required={true}
-          />
+            <Input
+              autoComplete="on"
+              paddingLeft="53.5px"
+              width="100%"
+              type="email"
+              value={fields.email}
+              handleChange={(e) => {
+                setFields({ ...fields, email: e.target.value });
+              }}
+              handleBlur={handleBlur("email")}
+              placeholder="E-mail"
+              required={true}
+            />
+          </div>
+          {touched.email && !fields.email && (
+            <p className={styles.inputError}>Required</p>
+          )}
         </div>
+        <div styles={styles.inputContainer}>
+          <div className={styles.inputWrapper}>
+            <FontAwesomeIcon icon={faLock} className={styles.inputIcon} />
 
-        <div className={styles.inputWrapper}>
-          <FontAwesomeIcon icon={faLock} className={styles.inputIcon} />
+            {eyeVisible && (
+              <VscEye
+                onClick={() => {
+                  toggleEyeVisible();
+                  toggleClosedEyeVisible();
+                  setType("text");
+                }}
+                size="24px"
+                className={styles.eyeIcon}
+              />
+            )}
 
-          {eyeVisible && (
-            <VscEye
-              onClick={() => {
-                toggleEyeVisible();
-                toggleClosedEyeVisible();
-                setType("text");
+            {closedEyeVisible && (
+              <VscEyeClosed
+                onClick={() => {
+                  toggleEyeVisible();
+                  toggleClosedEyeVisible();
+                  setType("password");
+                }}
+                size="24px"
+                className={styles.eyeIcon}
+              />
+            )}
+
+            <Input
+              autoComplete="on"
+              paddingLeft="53.5px"
+              width="100%"
+              type={type}
+              value={fields.password}
+              handleChange={(e) => {
+                setFields({ ...fields, password: e.target.value });
               }}
-              size="24px"
-              className={styles.eyeIcon}
+              handleBlur={handleBlur("password")}
+              placeholder="Password"
+              required={true}
             />
+          </div>
+          {touched.password && fields.password.length < 6 && (
+            <p className={styles.inputError}>
+              Password must be at least 6 characters!
+            </p>
           )}
-
-          {closedEyeVisible && (
-            <VscEyeClosed
-              onClick={() => {
-                toggleEyeVisible();
-                toggleClosedEyeVisible();
-                setType("password");
-              }}
-              size="24px"
-              className={styles.eyeIcon}
-            />
-          )}
-
-          <Input
-            autoComplete="on"
-            paddingLeft="53.5px"
-            width="100%"
-            type={type}
-            value={password}
-            handleChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            placeholder="Password"
-            required={true}
-          />
         </div>
         <div className={styles.buttonsContainer}>
           <Button variant="colored" type="submit">
