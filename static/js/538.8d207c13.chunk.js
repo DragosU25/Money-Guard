45fb29df31ed67bc8e0e91b396d7cdd68ca08e4d
turbomatrixxxl <turbomatrixxxl@users.noreply.github.com>
2024-09-09 08:09:07 +0000

    deploy: efb05d2c1fab2e6fec539f70d4dca0d6c56e5098

---
+++ static/js/538.8d207c13.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmoney_guard=self.webpackChunkmoney_guard||[]).push([[538],{538:(e,a,l)=>{l.d(a,{U_s:()=>t});var r=l(441);function t(e){return(0,r.k5)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z",clipRule:"evenodd"},child:[]}]})(e)}}}]);
+//# sourceMappingURL=538.8d207c13.chunk.js.map
