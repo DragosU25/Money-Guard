106a20029371765279cdd92029ae27e76efeda83
turbomatrixxxl <turbomatrixxxl@users.noreply.github.com>
2024-09-11 13:27:02 +0000

    deploy: e02579ea7f2c3c05d67db9c441cc3f22087514ad

---
+++ static/js/356.21bc5996.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmoney_guard=self.webpackChunkmoney_guard||[]).push([[356],{6356:(e,u,a)=>{a.r(u),a.d(u,{default:()=>r});a(5043);var n=a(579);function r(){return(0,n.jsx)("div",{children:"StatisticsTab"})}}}]);
+//# sourceMappingURL=356.21bc5996.chunk.js.map
